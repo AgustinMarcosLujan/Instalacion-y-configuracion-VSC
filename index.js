13f8b254494609382f0e3565b68fc23e08f8946f
Agustin Marcos Lujan <117218685+AgustinMarcosLujan@users.noreply.github.com>
2022-11-01 20:12:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+/* este es un comentario */
+
+function hola(nombre) {
+  return `hola ${nombre}`;
+}
+
+function card() {
+  return `
+  <article>
+  <article>
+  <img src="" alt="">
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime explicabo aspernatur aut error enim magni, quam praesentium ex necessitatibus perspiciatis corporis perferendis, molestias ipsam beatae quod amet vero quis repellendus!
+  </p>
+  </article>
+  </article>
+  
+  `;
+}
